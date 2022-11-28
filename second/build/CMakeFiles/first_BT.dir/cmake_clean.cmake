9b2ae99c4554dd5c0ca6335b193980eac517ba4b
Hero1451 <1136908271@qq.com>
2022-11-28 11:33:25 +0800

    Blackboard and ports

---
+++ second/build/CMakeFiles/first_BT.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/first_BT.dir/second_bt.cpp.o"
+  "first_BT"
+  "first_BT.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/first_BT.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ second/build/CMakeFiles/second_Bt.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/second_Bt.dir/second_bt.cpp.o"
+  "second_Bt"
+  "second_Bt.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/second_Bt.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
